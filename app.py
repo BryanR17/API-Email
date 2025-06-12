bd8fae941edfbc2a16f33ab83f7b74d5d77b30eb
Bryan Rodrigues <161602667+BryanR17@users.noreply.github.com>
2025-06-12 16:25:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import smtplib
+from email.message import EmailMessage
+
+app = Flask(__name__)
+CORS(app)  # Liberar requisições externas (ex: do frontend da Hostinger)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'API da Rodek está online'})
+
 @app.route('/enviar', methods=['POST'])
 def enviar():
     try:
